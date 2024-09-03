92a4d58ff56738e61be057a2da1b9f23569505e7
Nadia <nh894@nyu.edu>
2024-09-03 12:15:44 -0400

    books page added and linked

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function openMenu () {
 function closeMenu () {
     document.body.classList.remove('menu--open')
 }
+
+function goToBooks() {
+    window.location.href = `${window.location.origin}/books.html`
+}
